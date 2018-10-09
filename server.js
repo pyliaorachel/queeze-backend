2923da6d7320a47c368171c643c5d2bfead37f10
Peiyu Liao <pyliao@stanford.edu>
2018-10-09 16:10:06 -0700

    Read port from environment variable

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use(bodyParser.json());
 app.use('/', routes);
 
 // Start
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on port', process.env.PORT || 3000);
 });
